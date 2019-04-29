6b21450fe2212a049f3599ed0718adfc1999ca03
Peter Hewat <peter@Peters-iMac.local>
2019-04-29 11:22:23 +0200

    Dynamic routes

---
+++ pages/_username/following.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Users following {{ username }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: `User: ${this.username}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: ` You are watching users that are following ${this.username}`
+        }
+      ]
+    }
+  },
+  computed: {
+    username() {
+      return this.$route.params.username
+    }
+  }
+}
+</script>
---
+++ pages/_username/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>User {{ username }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: `User: ${this.username}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: ` You are watching user ${this.username}`
+        }
+      ]
+    }
+  },
+  computed: {
+    username() {
+      return this.$route.params.username
+    }
+  }
+}
+</script>
